350c30ccfb51d775a5bf2c189b3f2eb2ea87624b
abstractlyZach <zach3lee@gmail.com>
2025-04-15 16:45:00 -0700

    Remove mutable default

--- card_sorter/collections.py
+++ card_sorter/collections.py
@@ -11,8 +11,10 @@ class Collection(object):
 
     _cards: list[cards.Card]
 
-    def __init__(self, cards: list[cards.Card] = []):
-        self._cards = cards
+    def __init__(self, cards_=None):
+        if cards_ is None:
+            cards_ = []
+        self._cards = cards_
 
     def __str__(self):
         return "\n".join(str(card) for card in self._cards)
