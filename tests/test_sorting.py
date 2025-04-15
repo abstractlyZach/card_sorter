de641fdb3467e94116b35b1d0c55287fa8da559b
abstractlyZach <zach3lee@gmail.com>
2025-04-15 13:37:26 -0700

    Add description to parametrized tests

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -6,6 +6,7 @@ sort_test_parameters = (
         [
     ('', 'Isshin, Two Heavens as One', 'Marneus Calgar'),
     ('', 'Radha, Heir to Keld', 'Svella, Ice Shaper'),
+    ('intentionally wrong oops', 'Radha, Heir to Keld', 'Apple'),
 
             ]
         )
@@ -14,4 +15,4 @@ sort_test_parameters = (
 def test_sorting(description: str, expected_smaller: str, expected_larger:str):
     expected_smaller = cards.Card(expected_smaller)
     expected_larger = cards.Card(expected_larger)
-    assert expected_smaller < expected_larger
+    assert expected_smaller < expected_larger, description
