f0c1031b45f47ea9feff4a2be4928b346fd79fc2
abstractlyZach <zach3lee@gmail.com>
2025-04-15 22:46:35 -0700

    Separate card groupings into Collections and Packets

--- card_sorter/collections.py
+++ card_sorter/collections.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from . import cards
 
 
-class Collection(object):
+class CardGroup(object):
     """A collection of Cards.
 
     Assumes its input is already sorted.
@@ -17,7 +17,41 @@ class Collection(object):
         self._cards = cards_
 
     def __str__(self):
+        if not self._cards:
+            return "<No cards>"
         return "\n".join(str(card) for card in self._cards)
 
     def __eq__(self, other: Collection) -> bool:
         return self._cards == other._cards
+
+
+class Collection(CardGroup):
+    """A collection of Cards and Packets.
+
+    Assumes its input is already sorted.
+    """
+
+    _packets: list[Packet]
+
+    def __init__(self, cards_=None):
+        super().__init__(cards_)
+        self._packets = [Packet() for _ in range(len(self._cards) - 1)]
+
+    def __str__(self):
+        if not self._cards:
+            return "<No cards>"
+        lines = []
+        for i in range(len(self._packets)):
+            lines.append(str(self._cards[i]))
+            if not self._packets[i].is_empty():
+                lines.append("\t" + str(self._packets[i]))
+        lines.append(str(self._cards[-1]))
+        return "\n".join(lines)
+
+
+class Packet(CardGroup):
+    def sort(self):
+        self._cards.sort()
+
+    def is_empty(self) -> bool:
+        return not self._cards
