65b215589ff7cba841448c165ad1cf7a7094c0dc
abstractlyZach <zach3lee@gmail.com>
2025-04-20 14:39:00 -0700

    Add option to hide the collection from the CLI

--- card_sorter/console.py
+++ card_sorter/console.py
@@ -5,11 +5,13 @@ from card_sorter import cards, utils
 
 @click.command()
 @click.argument("csv_filename")
-def main(csv_filename: str):
+@click.option("--show-collection/--no-show-collection", default=False)
+def main(csv_filename: str, show_collection: bool):
     """The hypermodern Python project."""
     collection = utils.get_collection_from_csv(csv_filename)
-    # for card in collection:
-    #     click.echo(card)
+    if show_collection:
+        click.echo(collection)
+        click.echo("=" * 20)
     if cards.is_ordered(collection, error=True):
         click.echo("sorted!")
     else:
