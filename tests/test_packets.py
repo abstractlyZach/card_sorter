28d8e1da941dc950b24bbaeb5df07f5bbd1fce2a
abstractlyZach <zach3lee@gmail.com>
2025-04-20 17:21:14 -0700

    Test packet printing

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -2,6 +2,10 @@ import pytest
 
 from card_sorter import cards, utils
 
+cards_to_add_next_to_each_other = utils.get_collection_from_card_names(
+    ["Oliphaunt", "Olivia, Crimson Bride", "Ollenbock Escort"]
+)
+
 
 @pytest.fixture
 def small_collection():
@@ -19,6 +23,10 @@ add_card_test_cases = [
     (cards.Card("Oliphaunt")),
     (cards.Card("One with Nothing")),
 ]
+
+
+def collection_has_no_non_empty_packets(small_collection):
+    assert len(small_collection.get_packets()) == 0
 
 
 @pytest.mark.parametrize("new_card", add_card_test_cases)
@@ -56,4 +64,24 @@ def test_add_single_card_to_collection_packet_has_right_ordering(
     assert my_packet.card_after == expected_card_after
 
 
-# do the same, but check the packet left and right card
+def test_add_multiple_cards_to_a_packet(small_collection):
+    for card in cards_to_add_next_to_each_other:
+        small_collection.insert(card)
+    packets = small_collection.get_packets()
+    assert len(packets) == 1
+    my_packet = packets[0]
+    assert my_packet.card_before == cards.Card("Okiba Salvage")
+    assert my_packet.card_after == cards.Card("Omen of the Sea")
+    assert len(my_packet) == len(cards_to_add_next_to_each_other)
+
+
+def test_packet_printing(small_collection):
+    small_collection.insert(add_card_test_cases[0])
+    assert (
+        str(small_collection)
+        == """Okiba Salvage
+\tOliphaunt
+Omen of the Sea
+Oni-Cult Anvil
+Onward // Victory"""
+    )
