503df468a41fdf9bac31441660d06210458c6d5b
abstractlyZach <zach3lee@gmail.com>
2025-04-20 17:11:15 -0700

    Move test collection into a pytest fixture

--- tests/test_packets.py
+++ tests/test_packets.py
@@ -2,15 +2,19 @@ import pytest
 
 from card_sorter import cards, utils
 
+
 # set up as a fixture since it's mutable
-TEST_COLLECTION = utils.get_collection_from_card_names(
-    [
-        "Okiba Salvage",
-        "Omen of the Sea",
-        "Oni-Cult Anvil",
-        "Onward // Victory",
-    ]
-)
+@pytest.fixture
+def small_collection():
+    return utils.get_collection_from_card_names(
+        [
+            "Okiba Salvage",
+            "Omen of the Sea",
+            "Oni-Cult Anvil",
+            "Onward // Victory",
+        ]
+    )
+
 
 add_card_test_cases = [
     (cards.Card("Oliphaunt")),
@@ -32,17 +36,9 @@ ordering_test_cases = [
 
 
 @pytest.mark.parametrize("new_card", add_card_test_cases)
-def test_add_single_card_to_collection_shows_up_in_packet(new_card):
-    collection = utils.get_collection_from_card_names(
-        [
-            "Okiba Salvage",
-            "Omen of the Sea",
-            "Oni-Cult Anvil",
-            "Onward // Victory",
-        ]
-    )
-    collection.insert(new_card)
-    packets = collection.get_packets()
+def test_add_single_card_to_collection_shows_up_in_packet(small_collection, new_card):
+    small_collection.insert(new_card)
+    packets = small_collection.get_packets()
     assert new_card in packets[0]
 
 
@@ -50,18 +46,10 @@ def test_add_single_card_to_collection_shows_up_in_packet(new_card):
     "new_card, expected_card_before, expected_card_after", ordering_test_cases
 )
 def test_add_single_card_to_collection_packet_has_right_ordering(
-    new_card, expected_card_before, expected_card_after
+    small_collection, new_card, expected_card_before, expected_card_after
 ):
-    collection = utils.get_collection_from_card_names(
-        [
-            "Okiba Salvage",
-            "Omen of the Sea",
-            "Oni-Cult Anvil",
-            "Onward // Victory",
-        ]
-    )
-    collection.insert(new_card)
-    packets = collection.get_packets()
+    small_collection.insert(new_card)
+    packets = small_collection.get_packets()
     my_packet = packets[0]
     assert my_packet.card_before == expected_card_before
     assert my_packet.card_after == expected_card_after
